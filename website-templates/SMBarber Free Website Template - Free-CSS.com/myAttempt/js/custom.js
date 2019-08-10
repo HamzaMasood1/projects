86f66d69ac9f6939a8116f08bed53cb10e54d643
HamzaMasood1 <hamzamasood183@gmail.com>
2019-08-10 15:27:25 +0100

    first

--- website-templates/SMBarber Free Website Template - Free-CSS.com/myAttempt/js/custom.js
+++ website-templates/SMBarber Free Website Template - Free-CSS.com/myAttempt/js/custom.js
@@ -1,7 +1,13 @@
-/* ==============================================
-       LOADER -->
-    =============================================== */
-$(window).load(function(){
-    $('preloader').on(500).fadeout();
-    $('preloader').on(600).fadeout("slow");
-});
+(function ($) {
+    "use strict";
+    /* ==============================================
+           LOADER -->
+        =============================================== */
+    $(window).load(function () {
+        $("#preloader").on(500).fadeOut();
+        $(".preloader").on(600).fadeOut("slow");
+    });
+
+})(jQuery);
+
+
