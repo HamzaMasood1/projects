85dce8a060c20e6d0dd8104868f2c9ea54f16725
HamzaMasood1 <hamzamasood183@gmail.com>
2019-09-01 21:25:09 +0100

    Props

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,18 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-
-
-class App extends Component{
-    render(){
-        return(
-            <div className="App">
-                <h1>Hello, React!</h1>                
-            </div>            
-        )
-    }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById("root"));
+    
